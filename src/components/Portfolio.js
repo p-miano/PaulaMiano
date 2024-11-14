70abce85333c50b98e7b5b2c3a2c9978ef7906df
p-miano <103672587+p-miano@users.noreply.github.com>
2024-11-14 08:44:22 -0400

    Updated Portfolio section to use GitHub api information

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,139 +1,145 @@
 import React, { useState, useEffect } from 'react';
 
-const projectsData = [
-    {
-        id: 1,
-        title: "My Portfolio Website",
-        category: "Web",
-        description: "Responsive website built with React and Bootstrap, featuring dynamic sections and showcasing my skills and projects.",
-        githubLink: "https://github.com/p-miano/PaulaMiano.git",
-        deployedLink: "https://p-miano.github.io/PaulaMiano" 
-    },
-    {
-        id: 2,
-        title: "Hi Tech Distribution",
-        category: "Desktop",
-        description: "Windows application for managing inventory and orders.",
-        githubLink: "https://github.com/p-miano/MultiTier-HiTechDistributionManagementSystem.git",
-        deployedLink: null // No deployed link
-    },
-    {
-        id: 3,
-        title: "Tech Books",
-        category: "Web",
-        description: "Web application for managing books.",
-        githubLink: "https://github.com/p-miano/WebServerII-TechBooksV2.git",
-        deployedLink: null // No deployed link
-    },
-    {
-        id: 4,
-        title: "ToDo App",
-        category: "Mobile",
-        description: "Android application for managing tasks.",
-        githubLink: "https://github.com/p-miano/Android-TaskManager.git",
-        deployedLink: null // No deployed link
-    },
-    {
-        id: 5,
-        title: "City Info API",
-        category: "API",
-        description: "Web API designed to provide detailed information about cities and their points of interest.",
-        githubLink: "https://github.com/p-miano/CityInfoAPI.git",
-        deployedLink: null // No deployed link
-    },
-    {
-        id: 6,
-        title: "UI Showcase App",
-        category: "Mobile",
-        description: "Multi-page Android app showcasing various UI components and layouts, with support for dark mode and multi-language localization.",
-        githubLink: "https://github.com/p-miano/UIShowcaseApp.git",
-        deployedLink: null // No deployed link
-    },
-    {
-        id: 7,
-        title: "GitHub Proxy Server",
-        category: "API",
-        description: " Simple Node.js API server that acts as a proxy for fetching aggregated language data from my GitHub public and private repositories.",
-        githubLink: "https://github.com/p-miano/github-proxy-server",
-        deployedLink: "https://github-proxy-server.onrender.com/github-languages"
-    },
-    {
-        id: 8,
-        title: "Grocery Manager App",
-        category: "Mobile",
-        description: "Android application designed to help users manage their grocery items. Users can add, update, and delete grocery items. Uses Firebase Authentication for user management and SQLite for local data storage.",
-        githubLink: "https://github.com/p-miano/GroceryManager",
-        deployedLink: null // No deployed link
-    },
-];
-
-
 const Portfolio = () => {
-    const [selectedCategory, setSelectedCategory] = useState('All'); // State for the selected filter category
-    const [filteredProjects, setFilteredProjects] = useState(projectsData); // Projects filtered by category
+    const [selectedCategory, setSelectedCategory] = useState('All');
+    const [filteredProjects, setFilteredProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [sortOption, setSortOption] = useState('title'); // Track sort option
+    const [sortOrder, setSortOrder] = useState('asc'); // Track sort order
 
-    // Filter and sort projects based on the selected category
     useEffect(() => {
-        let projectsToDisplay = [...projectsData];
+        const fetchProjects = async () => {
+            try {
+                const response = await fetch('https://api.github.com/users/p-miano/repos');
+                const data = await response.json();
 
-        if (selectedCategory !== 'All') {
-            projectsToDisplay = projectsToDisplay.filter(project => project.category === selectedCategory);
+                const projects = data
+                    .filter(repo => repo.name.toLowerCase() !== 'p-miano') // Filter out your username project
+                    .map(repo => ({
+                        id: repo.id,
+                        title: addSpacesToTitle(repo.name), // Format title with spaces before capital letters
+                        category: getCategoryFromTopics(repo.topics),
+                        description: repo.description,
+                        githubLink: repo.html_url,
+                        deployedLink: repo.homepage || null,
+                        createdAt: new Date(repo.created_at).toLocaleDateString(),
+                        updatedAt: new Date(repo.updated_at).toLocaleDateString(),
+                        topics: repo.topics // Include topics for display on card
+                    }));
+
+                setFilteredProjects(projects);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            }
+        };
+
+        fetchProjects();
+    }, []);
+
+    const addSpacesToTitle = (title) => {
+        return title.replace(/([a-z])([A-Z])/g, '$1 $2');
+    };
+
+    const getCategoryFromTopics = (topics) => {
+        const categories = ['API', 'Web', 'Mobile', 'Desktop'];
+        for (let category of categories) {
+            if (topics.includes(category.toLowerCase())) {
+                return category;
+            }
         }
+        return 'Other';
+    };
 
-        // Sort by title
-        projectsToDisplay.sort((a, b) => a.title.localeCompare(b.title));
+    const sortProjects = (projects) => {
+        return projects.sort((a, b) => {
+            if (sortOption === 'title') {
+                return sortOrder === 'asc' ? a.title.localeCompare(b.title) : b.title.localeCompare(a.title);
+            } else if (sortOption === 'createdAt') {
+                return sortOrder === 'asc' 
+                    ? new Date(a.createdAt) - new Date(b.createdAt) 
+                    : new Date(b.createdAt) - new Date(a.createdAt);
+            } else if (sortOption === 'updatedAt') {
+                return sortOrder === 'asc' 
+                    ? new Date(a.updatedAt) - new Date(b.updatedAt) 
+                    : new Date(b.updatedAt) - new Date(a.updatedAt);
+            }
+            return 0;
+        });
+    };
 
-        setFilteredProjects(projectsToDisplay); // Set sorted and filtered projects
-    }, [selectedCategory]);
+    const filteredProjectsByCategory = filteredProjects.filter(project => {
+        return selectedCategory === 'All' || project.category === selectedCategory;
+    });
+
+    const sortedProjects = sortProjects(filteredProjectsByCategory);
 
     return (
         <section id="portfolio" className="p-5 bg-light">
             <div className="container">
-                {/* Portfolio Title */}
-                <h2 className="text-left">Portfolio</h2>
+                <h2 className="text-left text-pink">Portfolio</h2>
                 <div className="underline"></div>
-                {/* <p className="lead text-left">Explore my projects by category below:</p>*/}
 
-                {/* Filter Buttons */}
-                <div className="portfolio-filters mb-4">
-                    <button className={`btn ${selectedCategory === 'All' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('All')}>All</button>
-                    <button className={`btn ${selectedCategory === 'API' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('API')}>API</button>
-                    <button className={`btn ${selectedCategory === 'Desktop' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('Desktop')}>Desktop</button>
-                    <button className={`btn ${selectedCategory === 'Mobile' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('Mobile')}>Mobile</button>
-                    <button className={`btn ${selectedCategory === 'Web' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('Web')}>Web</button>
+                {/* Filter and Sort Container */}
+                <div className="filter-sort-container">
+                    {/* Main Category Filter Buttons */}
+                    <div className="portfolio-filters">
+                        <span>Filter by: </span>
+                        <button className={`btn ${selectedCategory === 'All' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('All')}>All</button>
+                        <button className={`btn ${selectedCategory === 'API' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('API')}>API</button>
+                        <button className={`btn ${selectedCategory === 'Desktop' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('Desktop')}>Desktop</button>
+                        <button className={`btn ${selectedCategory === 'Mobile' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('Mobile')}>Mobile</button>
+                        <button className={`btn ${selectedCategory === 'Web' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSelectedCategory('Web')}>Web</button>
+                    </div>
+
+                    {/* Divider */}
+                    <div className="divider"></div>
+
+                    {/* Sorting Options */}
+                    <div className="portfolio-sorting">
+                        <span>Sort by: </span>
+                        <button className={`btn ${sortOption === 'title' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSortOption('title')}>Title</button>
+                        <button className={`btn ${sortOption === 'createdAt' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSortOption('createdAt')}>Created Date</button>
+                        <button className={`btn ${sortOption === 'updatedAt' ? 'btn-primary' : 'btn-light'}`} onClick={() => setSortOption('updatedAt')}>Updated Date</button>
+                        <button onClick={() => setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc')} className="btn btn-outline-primary">
+                            {sortOrder === 'asc' ? '↑' : '↓'}
+                        </button>
+                    </div>
                 </div>
 
-                {/* Project Cards */}
-                <div className="row align-items-stretch">
-                    {filteredProjects.map(project => (
-                        <div className="col-md-4 mb-4" key={project.id}>
-                            <div className="card h-100 d-flex flex-column">
-                                <div className="card-body d-flex flex-column">
-                                    <h5 className="card-title">{project.title}</h5>
-                                    <p className="card-text">{project.description}</p>
+                {loading ? (
+                    <div>Loading...</div>
+                ) : (
+                    <div className="row align-items-stretch">
+                        {sortedProjects.map(project => (
+                            <div className="col-md-4 mb-4" key={project.id}>
+                                <div className="card h-100 d-flex flex-column">
+                                    <div className="card-body d-flex flex-column">
+                                        <h5 className="card-title text-pink">{project.title}</h5>
+                                        <p className="card-text">{project.description}</p>
+                                        {/* Display topics with text-pink class */}
+                                        <p><small><span className="text-pink font-weight-bold">Topics: </span>{project.topics.join(', ')}</small></p>
 
-                                    <div className="portfolio-buttons mt-auto">
-                                        {/* GitHub Link Button */}
-                                        <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary">
-                                            View on GitHub
-                                        </a>
-
-                                        {/* Deployed Site Button (if available) */}
-                                        {project.deployedLink && (
-                                            <a href={project.deployedLink} target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary">
-                                                Visit Site
+                                        <div className="portfolio-buttons mt-auto">
+                                            <a href={project.githubLink} target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary">
+                                                View on GitHub
                                             </a>
-                                        )}
+                                            {project.deployedLink && (
+                                                <a href={project.deployedLink} target="_blank" rel="noopener noreferrer" className="btn btn-outline-primary">
+                                                    Visit Site
+                                                </a>
+                                            )}
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                    ))}
-                </div>
-
+                        ))}
+                    </div>
+                )}
             </div>
         </section>
     );
-}
+};
 
 export default Portfolio;
